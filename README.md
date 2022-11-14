196e5778af6302cc714fc754bfc0e0f8d71469ad
cabreScrumMaster <101554405+cabreScrumMaster@users.noreply.github.com>
2022-11-14 20:45:09 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# Demo_Ansible_Terraform
+Demo_Ansible_Terraform
+Installation
+- Install Ansible
+    python3 -m pip install --user ansible
+
+- Install Terraform
+    sudo apt install terraform
+Configuration
+- Create a GCP project
+    Navigation menu > Project selector > New project
+
+- Create a service account
+    Navigation menu > IAM & Admin > Service accounts > Create a service account
+
+- Generate credentials 
+    Click on the created service account > Keys > Add key > Create a new key > JSON
+    Save the JSON file, rename it credentials.json and move it at the root of your project
+
+- Create a key pair 
+    Open a terminal and execute : ssh-keygen -t ed25519 -f ~/.ssh/ansible_ed25519 -C ansible
+    Copy and upload the public key to the service account (Compute Engine > Metadata > SSH keys > Add SSH key > paste the public key)
+
+- Download the project zip archive on the link : https://github.com/mounacqflo/Demo_Ansible_Terraform
+    Change the second line of the main.tf file with your own GCP project ID
+Launch
+- terraform init
+- terraform plan
+- terraform apply
+
+- wait the VM creation end
+
+- Access nginx on your browser with the output ip address 
+    nginx_ip = "xx.xxx.xxx.xxx"
+
+- Connect to the created VM instance and type the following commands to display the data put in database.sql that we finally stored in the database
+    sudo mysql -u root -p
+    enter the password: password
+    show databases;
+    use testdb;
+    select * from test;
+
+- terraform destroy (at the end to destroy the generated VM)
+Contributors
+Matthieu Cabrera, cabreramat@cy-tech.fr
+Aurélien Carmes, carmesaure@cy-tech.fr
+Florian Mounacq, mounacqflo@cy-tech.fr
+Titouan Riot, riottitoua@cy-tech.fr
--- main.tf
+++ main.tf
@@ -1,51 +1,28 @@
-/*provider "google" {
-    project = "terraform-demo-368116"
-    credentials = "./credentials.json"
-    region = "europe-west9"
-    zone = "europe-west9-a"
-}
-
-//europe-west9-a
-
-resource "google_compute_instance" "my_instance" {
-    name = "terraform-instance"
-    machine_type = "e2-micro"
-    zone = "europe-west9-a"
-    allow_stopping_for_update = true
-
-    boot_disk {
-        initialize_params {
-            //image = "debian-cloud/debian-10"
-            image ="ubuntu-2004-focal-v20211212"
-        }
-    }
-
-    network_interface {
-        network = "default"
-        access_config {
-            //necessary even empty
-        }
-    }
-}*/
-
 locals {
-  project_id       = "terraform-demo-368116"
+  # change the GCP project ID with your own
+  project_id       = "mon-premier-projet-365408"
+  # use the default network
   network          = "default"
+  # ubuntu image provided by GCP
   image            = "ubuntu-2004-focal-v20211212"
   ssh_user         = "ansible"
+  # path of the generated private key
   private_key_path = "~/.ssh/ansible_ed25519"
 }
 
+# contains resources we can use to create VMs
 provider "google" {
   project = local.project_id
   credentials = "./credentials.json"
   region  = "europe-west9"
 }
 
+# service account
 resource "google_service_account" "nginx" {
   account_id = "nginx-demo"
 }
 
+# firewall
 resource "google_compute_firewall" "web" {
   name    = "web-access"
   network = local.network
@@ -59,11 +36,13 @@ resource "google_compute_firewall" "web" {
   target_service_accounts = [google_service_account.nginx.email]
 }
 
+# create our VM instance
 resource "google_compute_instance" "nginx" {
   name         = "nginx"
   machine_type = "e2-micro"
   zone         = "europe-west9-a"
 
+  # use ubuntu as a boot disk 
   boot_disk {
     initialize_params {
       image = local.image
@@ -80,9 +59,11 @@ resource "google_compute_instance" "nginx" {
     scopes = ["cloud-platform"]
   }
 
+  # before run ansible playbook make sure VM is running
   provisioner "remote-exec" {
     inline = ["echo 'Wait until SSH is ready'"]
 
+    # establish SSH connection
     connection {
       type        = "ssh"
       user        = local.ssh_user
@@ -91,11 +72,14 @@ resource "google_compute_instance" "nginx" {
     }
   }
 
+  # use a local provisioner to run ansible
   provisioner "local-exec" {
     command = "ansible-playbook  -i ${google_compute_instance.nginx.network_interface.0.access_config.0.nat_ip}, --private-key ${local.private_key_path} nginx.yaml"
   }
+
 }
 
+# output the ip-address to the console
 output "nginx_ip" {
   value = google_compute_instance.nginx.network_interface.0.access_config.0.nat_ip
 }
